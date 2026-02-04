4838b361d4b2c3a2700f43d4564378c2e8ff8dbe
Yuxuan He <yh670@exeter.ac.uk>
2026-02-04 08:23:28 +0000

    update changes

--- AverageSquares.java
+++ AverageSquares.java
@@ -1,31 +1,22 @@
-import java.util.Scanner; // We use the Scanner class from java.util to handle user input
-
 public class AverageSquares {
     public static void main(String[] args) {
 
-        int startNum, endNum;
+        int startNum = Integer.parseInt(args[0]);
+        int endNum   = Integer.parseInt(args[1]);
 
-        if (args.length >= 2) {
-            startNum = Integer.parseInt(args[0]);
-            endNum   = Integer.parseInt(args[1]);
-        } else {
-            Scanner in = new Scanner(System.in);
-            startNum = in.nextInt();
-            endNum   = in.nextInt();
-        }
-        
-        int sum=0;
-        int i = startNum;      
-        while (i <=endNum) {
-            sum+=i*i;
+        double sum = 0;
+        int count = 0;
+
+        int i = startNum;
+        do {
+            sum += i * i;
+            count++;
             i++;
-        }
-/*         for(int i= startNum;i<= endNum;i++){
+        } while (i <= endNum);
 
-            sum+=i*i;
-        } */
-        System.out.println("Sum of squares from " + startNum + " to " + endNum + ":");
-        System.out.println(sum);
+        double average = sum / count;
 
+        System.out.println("Average of squares from " + startNum + " to " + endNum + ":");
+        System.out.println(average);
     }
 }
